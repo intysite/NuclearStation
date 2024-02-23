4f0497229c7d390a2ebe88c4007a9255c6cb6f98
Intisite <intysite@gmail.com>
2024-02-23 22:31:48 +0200

    Implementation of lazy initialization

--- src/main/java/org/javaacademy/NuclearStation.java
+++ src/main/java/org/javaacademy/NuclearStation.java
@@ -1,6 +1,5 @@
 package org.javaacademy;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
--- src/main/java/org/javaacademy/Runner.java
+++ src/main/java/org/javaacademy/Runner.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Runner {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
-        ReactorDepartment reactorDepartment = (ReactorDepartment) context.getBean("reactorDepartment");
         NuclearStation nuclearStation = (NuclearStation) context.getBean("nuclearStation");
         nuclearStation.start(3);
         System.out.println(nuclearStation.getTotalAmountOfEnergyGenerated());
