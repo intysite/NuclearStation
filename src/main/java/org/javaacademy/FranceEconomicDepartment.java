e2006262293bd5a95db995ebe1df913d280fbcc3
Intisite <intysite@gmail.com>
2024-02-25 21:15:12 +0200

    Start of economic development

---
+++ src/main/java/org/javaacademy/FranceEconomicDepartment.java
@@ -0,0 +1,10 @@
+package org.javaacademy;
+
+import java.math.BigDecimal;
+
+public class FranceEconomicDepartment extends EconomicDepartment {
+    @Override
+    public BigDecimal computeYearIncomes(long countElectricity) {
+        return null;
+    }
+}
