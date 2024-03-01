251a6d4fa160218bb6eda110fbadc944d361516a
Intisite <intysite@gmail.com>
2024-03-01 21:48:20 +0200

    Economic development

--- src/main/java/org/javaacademy/Runner.java
+++ src/main/java/org/javaacademy/Runner.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Runner {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
-        NuclearStation nuclearStation = (NuclearStation) context.getBean("nuclearStation");
+        NuclearStation nuclearStation = context.getBean(NuclearStation.class);
         nuclearStation.start(3);
         System.out.println(nuclearStation.getTotalAmountOfEnergyGenerated());
     }
