c28c63f766773d57efb0926cf4724bc18e8e3fa0
Intisite <intysite@gmail.com>
2024-03-02 22:51:17 +0200

    Economic development

--- src/main/java/org/javaacademy/FranceEconomicDepartment.java
+++ src/main/java/org/javaacademy/FranceEconomicDepartment.java
@@ -1,10 +1,27 @@
 package org.javaacademy;
 
-import java.math.BigDecimal;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+import java.util.stream.LongStream;
+
+@Component
+@Profile("france")
 public class FranceEconomicDepartment extends EconomicDepartment {
     @Override
     public BigDecimal computeYearIncomes(long countElectricity) {
-        return null;
+        BigDecimal baseRate = BigDecimal.valueOf(0.5);
+        long billion = 1_000_000_000;
+
+        BigDecimal incomeForBillions = LongStream.range(0, countElectricity / billion)
+                .mapToObj(i -> BigDecimal.valueOf(billion)
+                        .multiply(baseRate.multiply(BigDecimal.valueOf(Math.pow(0.99, i)))))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        BigDecimal incomeForRemaining = BigDecimal.valueOf(countElectricity % billion)
+                .multiply(baseRate.multiply(BigDecimal.valueOf(Math.pow(0.99, countElectricity / billion))));
+
+        return incomeForBillions.add(incomeForRemaining);
     }
 }
--- src/main/java/org/javaacademy/MoroccoEconomicDepartment.java
+++ src/main/java/org/javaacademy/MoroccoEconomicDepartment.java
@@ -1,10 +1,28 @@
 package org.javaacademy;
 
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
 import java.math.BigDecimal;
 
+@Component
+@Profile("morocco")
 public class MoroccoEconomicDepartment extends EconomicDepartment {
     @Override
     public BigDecimal computeYearIncomes(long countElectricity) {
-        return null;
+        BigDecimal baseRate = BigDecimal.valueOf(5);
+        BigDecimal increasedRate = BigDecimal.valueOf(6);
+        long fiveBillion = 5_000_000_000L;
+
+        BigDecimal income;
+        if (countElectricity <= fiveBillion) {
+            income = baseRate.multiply(BigDecimal.valueOf(countElectricity));
+        } else {
+            long remainingKilowattHours = countElectricity - fiveBillion;
+            income = baseRate.multiply(BigDecimal.valueOf(fiveBillion))
+                    .add(increasedRate.multiply(BigDecimal.valueOf(remainingKilowattHours)));
+        }
+
+        return income;
     }
 }
